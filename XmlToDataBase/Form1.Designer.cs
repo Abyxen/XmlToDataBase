065098f89fc7c3540d7d4c9adc2c932663ffb2ac
Abyxen <abiraldawadi1@gmail.com>
2026-06-01 10:31:18 +0100

    Add project files.

---
+++ XmlToDataBase/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace XmlToDataBase
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            openFileDialog1 = new OpenFileDialog();
+            saveFileDialog1 = new SaveFileDialog();
+            btnLoad = new Button();
+            btnDelete = new Button();
+            btnRestore = new Button();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnLoad
+            // 
+            btnLoad.Location = new Point(399, 116);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(75, 23);
+            btnLoad.TabIndex = 0;
+            btnLoad.Text = "Load";
+            btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(252, 326);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(75, 23);
+            btnDelete.TabIndex = 1;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnRestore
+            // 
+            btnRestore.Location = new Point(435, 326);
+            btnRestore.Name = "btnRestore";
+            btnRestore.Size = new Size(75, 23);
+            btnRestore.TabIndex = 2;
+            btnRestore.Text = "Restore";
+            btnRestore.UseVisualStyleBackColor = true;
+            btnRestore.Click += button3_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 386);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(577, 405);
+            dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.Screenshot_2026_05_27_222906;
+            ClientSize = new Size(613, 819);
+            Controls.Add(dataGridView1);
+            Controls.Add(btnRestore);
+            Controls.Add(btnDelete);
+            Controls.Add(btnLoad);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private OpenFileDialog openFileDialog1;
+        private SaveFileDialog saveFileDialog1;
+        private Button btnLoad;
+        private Button btnDelete;
+        private Button btnRestore;
+        private DataGridView dataGridView1;
+    }
+}
